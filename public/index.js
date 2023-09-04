8a8b9584cec7d026166db2cd08b0f497f86661d7
Sobayo <sobayotoby@gmail.com>
2023-09-04 16:44:00 +0100

    mastering tailwind

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('#burger')
+const menu = document.querySelector('#menu')
+
+btn.addEventListener('click', () => {
+	if (menu.classList.contains('hidden')){
+		menu.classList.remove('hidden')
+	} else {
+		menu.classList.add('hidden')
+	}
+})
